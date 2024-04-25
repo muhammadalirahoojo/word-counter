799a85898a58f57372c81f95e2be9a0b0b8d6b8b
muhammadalirahoojo <malirahoojo79@gmail.com>
2024-04-25 08:20:41 +0500

    first commit of my word counter

---
+++ word_counter.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word:"
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+// print array of words on console. 
+console.log(words);
+// print the word count of the sentence to the console.
+console.log(`Your Sentence Word Counr is: ${words.length}`);
---
+++ word_counter.ts
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const answers:{
+    Sentence:string
+}= await inquirer.prompt([
+    {
+        name:"Sentence",
+        type:"input",
+        message:"Enter your sentence to count the word:"
+}
+])
+
+const words=answers.Sentence.trim().split(" ");
+// print array of words on console. 
+console.log(words);
+// print the word count of the sentence to the console.
+console.log(`Your Sentence Word Counr is: ${words.length}`);
+
+
